6c7352b23de4095dbe5db0325df2f0f672095268
wta12 <cnkhanh1986@gmail.com>
2022-02-12 15:47:59 +0000

    test run ok and add ccache

--- Docker/postCreateCmd.sh
+++ Docker/postCreateCmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Hello World"
 ls -la 
-echo "test"
 python3 --help
+# git config --local user.name wta12
+# git config --local user.email cnkhanh1986@gmail.com
+# git config --local core.autocrlftrue
+echo "test Done"
